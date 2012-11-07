d5c24de49a59f1583018111002c1f9e70b29f98e
MonodetH <monodeth@gmail.com>
2012-11-07 01:23:08 -0300

    Creacion de directorios

---
+++ web/config.php
@@ -0,0 +1,16 @@
+<?php
+	## BD ##
+	$bd['host'] = 'localhost';
+	$bd['user'] = 'tarea3';
+	$bd['pass'] = 'x';
+	$bd['bd'] = 'tarea3';
+
+	## VARIABLES GLOBALES ##   			//no se si estan bn aun
+	$path['web']=getcwd().'/web/';
+	$path['modelo']=getcwd().'/web/model/';
+	$path['vista']=getcwd().'/web/view/';
+	$path['control']=getcwd().'/web/controller/';
+	$path['css']=getcwd().'/web/css/';
+	$path['images']=getcwd().'/web/images/';
+	$path['url']='http://'.$_SERVER['HTTP_HOST'].'/tarea3/';
+?>
